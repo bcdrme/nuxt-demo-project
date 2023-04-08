36e8cbe16d7a3c290dcfed03e218bd74158ac1be
Benoit Coudour <benoit@oplit.fr>
2023-04-08 22:56:30 +0200

    Dummy index and user pages

---
+++ pages/users/[...id].vue
@@ -0,0 +1,15 @@
+<script setup>
+const route = useRoute();
+const { data } = await useFetch(
+  `https://jsonplaceholder.typicode.com/users/${route.params.id}`
+);
+</script>
+
+<template>
+  <div
+    class="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 h-screen"
+  >
+    <h1>User page</h1>
+    <p>{{ data.name }}</p>
+  </div>
+</template>
