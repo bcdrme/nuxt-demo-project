ded0ae6c189301ea9b782fa7bf40a0df0ed27153
Benoit Coudour <benoit.coudour@gmail.com>
2023-04-09 15:40:02 +0200

    Prettier config

--- pages/users/[...id].vue
+++ pages/users/[...id].vue
@@ -1,14 +1,10 @@
 <script setup>
-const route = useRoute();
-const { data } = await useFetch(
-  `https://jsonplaceholder.typicode.com/users/${route.params.id}`
-);
+const route = useRoute()
+const { data } = await useFetch(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
 </script>
 
 <template>
-  <div
-    class="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 h-screen"
-  >
+  <div class="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 h-screen">
     <h1>User page</h1>
     <p>{{ data.name }}</p>
   </div>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,11 +1,9 @@
 <script setup>
-const { data } = await useFetch("https://jsonplaceholder.typicode.com/users");
+const { data } = await useFetch('https://jsonplaceholder.typicode.com/users')
 </script>
 
 <template>
-  <div
-    class="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 h-screen"
-  >
+  <div class="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 h-screen">
     <h1>Users</h1>
     <ul>
       <li v-for="user in data" :key="user.id">
